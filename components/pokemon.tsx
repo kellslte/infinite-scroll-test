58d3914d1c9d756cc87f72de42d6c574e43b3cb5
Favour Max-Oti <maxotif@gmail.com>
2024-02-22 18:11:04 +0100

    initial commit

---
+++ components/pokemon.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import React from 'react'
+
+type Props = {
+    name: string;
+    image: string;
+    innerRef?: (node ?: Element | null | undefined) => void;
+}
+
+const Pokemon: React.FC<Props> = ({name, image, innerRef}) => {
+    return (
+        <React.Fragment>
+            <div className='' ref={innerRef}
+            >
+                <div className='flex flex-row cursor-pointer shadow-lg bg-white border-r-4  gap-4 items-center p-2 rounded'>
+                    <div className='rounded-full w-[4.5rem] h-[4.5rem]'>
+                        <Image src={image} alt='profileImg' width={200} height={200} className='rounded-full object-cover' />
+                    </div>
+                    <div><span>{name}</span></div>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Pokemon
